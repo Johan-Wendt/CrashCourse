7056fc3f5f989ee3e98d0110ebc9119ac086e85a
Johan Wendt <johan.wendt@telavox.se>
2015-11-19 22:21:11 +0100

    first commit

---
+++ src/crashcourse/MainPopup.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crashcourse;
+
+/**
+ *
+ * @author johanwendt
+ */
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
+
+/**
+ *This class creates the stage that lets the player initiate a new game.
+ * @author johanwendt
+ */
+public class MainPopup extends PopUp{
+ //   private final static ComboBox chooseNumberOfPlayers = new ComboBox<>();
+    private static final Label winnerInfo = new Label();
+    private CrashCourse crashCourse;
+    
+    
+    private static final GridPane controlsPane = new GridPane();
+
+    
+    
+    
+    
+    public MainPopup(String title, String infoText, String okMessage, String cancelMessage, CrashCourse crashCourse) {
+        super(title, infoText, okMessage, cancelMessage, PopUp.STANDARD_PANE_WIDTH);
+        this.crashCourse = crashCourse;
+        
+        getOkButton().setDisable(true);
+        getCancelButton().setDisable(true);
+     //   setUpWinnerInfo(null, false);
+        addComboBox();
+        setUpLowerPart();
+        addExtraPane(setUpMenuShortcuts());
+        showPopUp(true);
+    }
+    private void addComboBox() {
+        ObservableList<String> options = FXCollections.observableArrayList("1 player", "2 players","3 players","4 players");
+        ComboBox chooseNumberOfPlayers = new ComboBox<>();
+        chooseNumberOfPlayers.getItems().addAll(options);
+        chooseNumberOfPlayers.setValue("Select number of players");
+        chooseNumberOfPlayers.setPrefWidth(260);
+        
+        chooseNumberOfPlayers.setOnAction(e -> {
+    //        GameEngine.setNumberOfPlayers(options.indexOf(chooseNumberOfPlayers.getValue()) + 1);
+            getOkButton().setDisable(false);
+        });
+        chooseNumberOfPlayers.setOnKeyPressed(e -> {
+            if(e.getCode().equals(KeyCode.ENTER) && !chooseNumberOfPlayers.getValue().equals("Select number of players")) {
+        //        gameEngine.restart();
+                showPopUp(false);
+                getCancelButton().setDisable(false);
+            }
+        });
+        addExtraButton(chooseNumberOfPlayers);
+    }
+    @Override
+    protected void setOnActions() {
+        getOkButton().setOnAction(e -> {
+            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000),ae -> 
+            startGame()
+            ));
+            
+           // UserInterface.setDisableMenuBar(false);
+            showPopUp(false);
+            getCancelButton().setDisable(false);
+        });
+
+        getCancelButton().setOnAction(e -> {
+            showPopUp(false);
+        });   
+        
+    }
+    private void startGame() {
+   //     gameEngine.restart();
+           // UserInterface.setDisableMenuBar(false);
+            showPopUp(false);
+            getCancelButton().setDisable(false);
+    }
+    //Add info about the objective of the game and info about the winner. 
+        //Winner info is empty untill a player has won the game and is
+        //emptied again after the game is restarted.
+    /**
+    public void setUpWinnerInfo(PlayerEnum winner, boolean noScore) {
+        Label infoLabel = getExtraLabel();
+        if(noScore) {
+            infoLabel.setText("You lost your flow!");
+            infoLabel.setTextFill(UserInterface.infoColor());
+        }
+        else if(winner == null) {
+            infoLabel.setText("Good luck!");
+            infoLabel.setTextFill(UserInterface.infoColor());
+            infoLabel.setFont(new Font(30));
+        }
+        else {
+            infoLabel.setTextFill(winner.getScoreColor());
+            infoLabel.setText("The winner is " + winner.getName());                    
+        }
+    }
+    **/
+    public HBox setUpMenuShortcuts() {
+        
+        HBox innerButtonPane = new HBox();
+        
+        final Button controllButton = new Button("Controlls");
+        final Button ruleButton = new Button("  Rules  ");
+        final Button quitButton = new Button("   Quit  ");
+        
+        controllButton.setStyle("-fx-base: #01c9f3; -fx-font-size: 15px;");
+        ruleButton.setStyle("-fx-base: #01c9f3; -fx-font-size: 15px;");
+        quitButton.setStyle("-fx-base: #01c9f3; -fx-font-size: 15px;");
+        
+        controllButton.setMinWidth(150);
+        ruleButton.setMinWidth(150);
+        ruleButton.setMinWidth(150);
+        
+        controllButton.setPadding(new Insets(PopUp.getStandardPadding() * 3));
+        ruleButton.setPadding(new Insets(PopUp.getStandardPadding() * 3));
+        quitButton.setPadding(new Insets(PopUp.getStandardPadding() * 3));
+        
+        controllButton.setMinWidth(100);
+        ruleButton.setMinWidth(100);
+        ruleButton.setMinWidth(100);
+        
+        
+        HBox.setMargin(ruleButton, new Insets(0, PopUp.getStandardPadding() / 4, 0, PopUp.getStandardPadding() / 4));
+        
+        controllButton.setOnAction(e -> {
+      //      UserInterface.showControllStage(true);
+        });
+        ruleButton.setOnAction(e -> {
+     //       UserInterface.showRulesStage(true);
+        });
+        quitButton.setOnAction(e -> {
+      //      UserInterface.showQuitStage(true);
+        });
+        
+        innerButtonPane.getChildren().addAll(controllButton, ruleButton, quitButton);
+        
+        return innerButtonPane;
+    }
+}
---
+++ src/crashcourse/PopUp.java
@@ -0,0 +1,230 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crashcourse;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *Class that is the base for all popup, that is all the stages except for the
+ * main stage where the game is played.
+ * @author johanwendt
+ */
+public abstract class PopUp {
+    
+    private final Stage pupUpStage = new Stage();
+    private Scene popUpScene;
+    private final Label infoLabelText = new Label();
+    private Label extraLabel = new Label();
+    private final Button okButton = new Button();
+    private final Button cancelButton = new Button();
+    
+    
+    private final VBox popUpPane = new VBox();
+    private final VBox popUpPaneInnerUpper = new VBox();
+    private final HBox popUpPaneInnerLower = new HBox();
+   // private HBox extraPane = new HBox();
+    
+    protected final static int STANDARD_PANE_WIDTH = 600;
+    private final static int STANDARD_PANE_HEIGHT = 600;
+    private final static int INNER_UPPER_HEIGHT = STANDARD_PANE_HEIGHT - 50;
+    private final static int INNER_LOWER_HEIGHT = STANDARD_PANE_HEIGHT - INNER_UPPER_HEIGHT;
+    private final static int STANDARD_PADDING = 20;
+    
+    protected final static int STANDARD_BUTTON_HEIGHT = 70;
+    
+    /**
+     * Create a popup with a title, a message for the ok button and a preffered 
+     * width for the pane.
+     * @param title title of the stage.
+     * @param okMessage message displayed on the confirmation button.
+     * @param paneWidth prefered width of the pane
+     */
+    public PopUp(String title, String okMessage, int paneWidth) {
+        setUpOuterPart(title, paneWidth);
+        setUpLowerPart();
+        setUpExtraPane();
+        setUpUpperPart();
+        addOkButton(okMessage);
+        setStandardOnStageKeyBehaviour();
+        setOnActions();
+        okButton.requestFocus();
+    }
+    public PopUp(String title, String infoText, String okMessage, int paneWidth) {
+        setUpOuterPart(title, paneWidth);
+        setUpLowerPart();
+        setUpExtraPane();
+        setUpUpperPart();
+        setUpInfoLabel(infoText);
+        addOkButton(okMessage);
+        setStandardOnStageKeyBehaviour();
+        setOnActions();
+        okButton.requestFocus();
+    }
+    public PopUp(String title, String infoText, String okMessage, String cancelMessage, int paneWidth) {
+        setUpOuterPart(title, paneWidth);
+        setUpLowerPart();
+        setUpExtraPane();
+        setUpUpperPart();
+        setUpInfoLabel(infoText);
+        addOkButton(okMessage);
+        addCancelButton(cancelMessage);
+        setStandardOnStageKeyBehaviour();
+        setOnActions();
+        okButton.requestFocus();
+    }
+    private void setUpOuterPart(String title, int paneWidth) {
+        pupUpStage.setTitle(title);
+        pupUpStage.setAlwaysOnTop(true);
+   //     pupUpStage.initOwner(UserInterface.getBattleStage());
+        pupUpStage.setOnCloseRequest(c -> {
+            showPopUp(false);
+        });
+        setBackGround("test.jpg", true);
+                
+        popUpPane.getChildren().add(popUpPaneInnerUpper);
+       // popUpPane.getChildren().add(extraPane);
+        popUpPane.getChildren().add(popUpPaneInnerLower);
+        
+        popUpScene = new Scene(popUpPane, paneWidth, STANDARD_PANE_HEIGHT);
+        pupUpStage.setScene(popUpScene);
+        pupUpStage.setResizable(false);
+    }
+    protected void setUpUpperPart() { 
+        popUpPaneInnerUpper.setPadding(new Insets(STANDARD_PADDING / 2, STANDARD_PADDING, STANDARD_PADDING / 4, STANDARD_PADDING));
+        popUpPaneInnerUpper.setPrefHeight(INNER_UPPER_HEIGHT);
+        popUpPaneInnerUpper.getChildren().add(infoLabelText);
+    }
+    protected void setUpExtraPane() {
+        HBox extraPane = new HBox();
+        popUpPane.getChildren().add(1, extraPane);
+        extraPane.setAlignment(Pos.CENTER);
+        extraPane.getChildren().add(extraLabel);
+    }
+    protected void setUpLowerPart() {
+        popUpPaneInnerLower.setPrefHeight(INNER_LOWER_HEIGHT);
+        popUpPaneInnerLower.setAlignment(Pos.CENTER_RIGHT);
+        popUpPaneInnerLower.setPadding(new Insets(STANDARD_PADDING));
+        HBox.setMargin(okButton, new Insets(0, STANDARD_PADDING / 4, 0, STANDARD_PADDING / 4));
+
+    }
+    protected void setUpInfoLabel(String infoText) {
+        infoLabelText.setText(infoText);
+        infoLabelText.setWrapText(true);
+        infoLabelText.setStyle("-fx-font-size: 25px; -fx-text-fill: #2B2B22");
+        VBox.setMargin(infoLabelText, new Insets(0, 0, STANDARD_PADDING, 0));
+        
+    }
+    protected  void setInfoText(String infoText) {
+        getInfoLabel().setText(infoText);
+    }       
+    protected void showPopUp(boolean show) {
+        if(show && !pupUpStage.isShowing()) {
+            pupUpStage.show();
+   //         GameEngine.popUpOpened();
+        }
+        if(!show && pupUpStage.isShowing()) {
+            pupUpStage.hide();
+   //         GameEngine.popUpClosed();
+        }
+    }
+    protected void addOkButton(String okMessage) {
+        okButton.setText(okMessage);
+        okButton.setPrefWidth(okMessage.length() * 9);
+        okButton.setStyle("-fx-base: #009933;");
+        okButton.setPrefHeight(STANDARD_BUTTON_HEIGHT);
+        popUpPaneInnerLower.getChildren().add(getOkButton());
+    }
+    protected void addCancelButton(String cancelMessage) { 
+        cancelButton.setText(cancelMessage);
+        cancelButton.setCancelButton(true);
+        cancelButton.setPrefWidth(cancelMessage.length() * 9);
+        cancelButton.setStyle("-fx-base: #FF3300;");
+        cancelButton.setPrefHeight(STANDARD_BUTTON_HEIGHT);
+        popUpPaneInnerLower.getChildren().add(getCancelButton());
+    }
+    protected void addExtraButton(Control extraButton) {
+        extraButton.setStyle("-fx-base: #0000FF; -fx-font-size: 15px;");
+        extraButton.setPrefHeight(STANDARD_BUTTON_HEIGHT);
+        popUpPaneInnerLower.getChildren().add(0, extraButton);
+    }
+    protected void addExtraButton(Control extraButton, int StringLength) {
+        extraButton.setPrefWidth(StringLength * 9);
+        extraButton.setStyle("-fx-base: #0000FF; -fx-font-size: 15px;");
+        extraButton.setPrefHeight(STANDARD_BUTTON_HEIGHT);
+        popUpPaneInnerLower.getChildren().add(0, extraButton);
+    }
+    protected Label getInfoLabel() {
+        return infoLabelText;
+    }
+    protected Button getOkButton() {
+        return okButton;
+    }
+    protected Button getCancelButton() {
+        return cancelButton;
+    }
+    public VBox getPopUpPane() {
+        return popUpPane;
+    }
+    protected VBox getPopUpPaneInnerUpper() {
+        return popUpPaneInnerUpper;
+    }
+    protected HBox getPopUpPaneInnerLower() {
+        return popUpPaneInnerLower;
+    }
+    protected void setStandardOnStageKeyBehaviour() {
+        okButton.defaultButtonProperty().bind(okButton.focusedProperty());
+        cancelButton.defaultButtonProperty().bind(cancelButton.focusedProperty());
+    }
+    /**
+     *Set actions for pressing the buttons. The canelButton usually just closes 
+     * the popup while the okButton closes the popup and performs some
+     * action.
+     */
+    protected abstract void setOnActions();
+    
+    protected void addInfoPicture(ImageView picture) {
+        extraLabel.setGraphic(picture);
+    }
+    protected Label getExtraLabel() {
+        return extraLabel;
+    }
+    protected void setBackGround(String name, boolean cover) {
+        Image image = new Image(getClass().getResourceAsStream(name));
+        // new BackgroundSize(width, height, widthAsPercentage, heightAsPercentage, contain, cover)
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, cover);
+        // new BackgroundImage(image, repeatX, repeatY, position, size)
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        // new Background(images...)
+        Background background = new Background(backgroundImage);
+        popUpPane.setBackground(background);
+    }
+    protected void addExtraPane(Pane pane) {
+        popUpPaneInnerUpper.getChildren().add(pane);
+        popUpPaneInnerUpper.setAlignment(Pos.TOP_CENTER);
+    }
+    protected Stage getPopUpStage() {
+        return pupUpStage;
+    }
+    protected static int getStandardPadding() {
+        return STANDARD_PADDING;
+    }
+}
